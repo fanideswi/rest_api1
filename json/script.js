069e5071ccbf47376870106a9d35fefea8faf116
fanideswi <fanitambunan03@gmail.com>
2025-05-29 16:18:57 +0700

    menggunakan JQUERY

--- json/script.js
+++ json/script.js
@@ -6,13 +6,20 @@
 
 // console.log(JSON.stringify(mahasiswa));
 
+
+
 // JSON KE OBJEK
-let xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-       let mahasiswa = JSON.parse(this.responseText);
-       console.log(mahasiswa);
-    }
-}
-xhr.open('GET','coba.json',true);
-xhr.send();
+// let xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function() {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//        let mahasiswa = JSON.parse(this.responseText);
+//        console.log(mahasiswa);
+//     }
+// }
+// xhr.open('GET','coba.json',true);
+// xhr.send();
+
+$.getJSON('coba.json', function(data) {
+    console.log(data);
+
+});
