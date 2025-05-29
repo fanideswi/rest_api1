0606fba774f5766cb24277d2ae16137aeb4ceb23
fanideswi <fanitambunan03@gmail.com>
2025-05-29 20:41:53 +0700

    update tampilan movie 1

--- wpu_movie/js/script.js
+++ wpu_movie/js/script.js
@@ -1,5 +1,6 @@
-$('#search-button').on('click', function() {
-
+function searchMovie(){
+     $('#movie-list').html('');
+        
     $.ajax({
         url: 'http://omdbapi.com',
         type: 'get',
@@ -27,11 +28,58 @@ $('#search-button').on('click', function() {
                  `);
             }); 
 
+            $('#search-input').val('');
+
             } else {
                 $('#movie-list').html(`
                     <div class="col">
                         <h1 class="text-center">` + result.Error + `</h1>
                     </div>
+                `);
+            }
+        }
+    });
+}
+$('#search-button').on('click', function() {
+    searchMovie();   
+});
+
+$('#search-input').on('keyup', function(e) {
+    if(e.which === 13) {
+        searchMovie();
+    }
+});
+
+$('#movie-list').on('click', '.see-detail', function () {
+       $.ajax({
+        url: 'http://omdbapi.com',
+        dataType: 'json',
+        type: 'get',
+        data:{
+            'apikey': '45aca54c', 
+            'i': $(this).data('id')
+        },
+                success: function (movie) {
+            if(movie.Response === "True") {
+
+                $('.modal-body').html (`
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class = "col-md-4">
+                                <img src="`+ movie.Poster +`" class= "img-fluid">
+                            </div>
+
+                            <div class="col-md-8">
+                                <ul class="list-group">
+                                    <li class="list-group-item"><h3>`+ movie.Title +`</h3></li>
+                                    <li class="list-group-item">Released : `+ movie.Released+`</li>
+                                    <li class="list-group-item">Genre : `+ movie.Genre +`</li>
+                                    <li class="list-group-item">Director : `+ movie.Director +`</li>
+                                    <li class="list-group-item">Actors : `+ movie.Actors+`</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>  
                 `);
             }
         }
