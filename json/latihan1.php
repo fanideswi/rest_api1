fa8a94700f843bf92ec96a7a8df56bd50b79090c
fanideswi <fanitambunan03@gmail.com>
2025-05-29 15:47:14 +0700

    mengubah rey ke json menggunakan database

--- json/latihan1.php
+++ json/latihan1.php
@@ -1,17 +1,24 @@
 <?php 
 
-$mahasiswa = [
-    [
-        "nama" => "Fani Deswi Sapitri Tambunan",
-        "nim" => "2217020011",
-        "email" => "fanitambunan03@gmail.com"
-    ],
-      [
-        "nama" => "Arfansyah",
-        "nim" => "2217020012",
-        "email" => "Arfansyah@gmail.com"
-    ],
-];
+// $mahasiswa = [
+//     [
+//         "nama" => "Fani Deswi Sapitri Tambunan",
+//         "nim" => "2217020011",
+//         "email" => "fanitambunan03@gmail.com"
+//     ],
+//     [
+//         "nama" => "Arfansyah",
+//         "nim" => "2217020012",
+//         "email" => "Arfansyah@gmail.com"
+//     ],
+// ];
+
+$dbh = new PDO('mysql:host=localhost;dbname=db_mahasiswa', 'root', 'root');
+$db = $dbh->prepare('SELECT * FROM mahasiswa');
+$db->execute();
+$mahasiswa = $db->fetchAll(PDO::FETCH_ASSOC);
+
+
 
 $data = json_encode($mahasiswa);
 echo $data;
