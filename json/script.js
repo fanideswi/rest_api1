3795608a08a1837b3f7b1ccc02cc24c84d61f52e
fanideswi <fanitambunan03@gmail.com>
2025-05-29 16:15:33 +0700

    JSON menjadi OBJECT

--- json/script.js
+++ json/script.js
@@ -1,7 +1,18 @@
-let mahasiswa = {
-     nama : "fani tambunan",
-     nim : "2217020011",
-     email : "fanitambunan03@gmail.com"
-}
+// let mahasiswa = {
+//      nama : "fani tambunan",
+//      nim : "2217020011",
+//      email : "fanitambunan03@gmail.com"
+// }
 
-console.log(JSON.stringify(mahasiswa));
+// console.log(JSON.stringify(mahasiswa));
+
+// JSON KE OBJEK
+let xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+       let mahasiswa = JSON.parse(this.responseText);
+       console.log(mahasiswa);
+    }
+}
+xhr.open('GET','coba.json',true);
+xhr.send();
