4cdcea7d7c8188fb687706f13e3f8a0d2b303339
fanideswi <fanitambunan03@gmail.com>
2025-05-29 16:50:40 +0700

    update tampilan menu

--- wpu_hut/latihan1.php
+++ wpu_hut/latihan1.php
@@ -1,3 +1,12 @@
+<!-- mengambil file dari folder yang sama -->
+<?php
+$data = file_get_contents('data/pizza.json');
+$menu = json_decode($data, true);
+
+$menu = $menu["menu"];
+?>
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -12,20 +21,47 @@
   </head>
   <body>
  
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-link" href="#">Features</a>
-      <a class="nav-link" href="#">Pricing</a>
-      <a class="nav-link disabled">Disabled</a>
-    </div>
-  </div>
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class ="container">
+
+        <a class="navbar-brand" href="#">
+          <img src="img/logo.png" width="120">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link active" href="#">All Menu </a>
+          </div>
+        </div>
+</div>
 </nav>
+
+<div class ="container">
+
+    <div class="row mt-3">
+        <div class ="col">
+            <h1>All Menu </h1>
+        </div>
+    </div>
+
+    <div class ="row">
+        <?php foreach ($menu as $row) : ?>
+        <div class="col-md-4">
+            <div class="card mb-3">
+                <img src="img/menu/<?= $row["gambar"];?>" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $row["nama"];?></h5>
+                    <p class="card-text"><?= $row["deskripsi"];?></p>
+                    <h5 class="card-title">Rp. <?= $row["harga"];?></h5>          
+                    <a href="#" class="btn btn-primary">Pesan Sekarang</a>
+                </div>
+            </div>
+        </div>
+        <?php endforeach; ?>
+    </div>
+</div>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
