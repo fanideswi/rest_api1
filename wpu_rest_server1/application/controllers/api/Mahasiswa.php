4fe59e1ae6a1353eedf8547d30d6768454580238
fanideswi <fanitambunan03@gmail.com>
2025-05-30 18:03:03 +0700

    menambahkan kontroler dan model mahasiswa

--- wpu_rest_server1/application/controllers/api/Mahasiswa.php
+++ wpu_rest_server1/application/controllers/api/Mahasiswa.php
@@ -7,18 +7,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH . 'libraries/REST_Controller.php';
 require APPPATH . 'libraries/Format.php';
 
-class Mahasiswa extends REST_Controller
+class Mahasiswa extends REST_Controller 
 {
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('Mahasiswa_model', 'mahasiswa');
+        $this->load->model('Mahasiswa_model');
     }
 
     public function index_get()
     {
-        $mahasiswa = $this->mahasiswa->getMahasiswa();
-        
-        
+        $mahasiswa =$this-> Mahasiswa_model->getMahasiswa();
+        var_dump($mahasiswa);
     }
 }
