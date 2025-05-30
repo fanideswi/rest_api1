6fcbdd965db4ed5714f604aca8e2d0f5b72484c1
fanideswi <fanitambunan03@gmail.com>
2025-05-30 07:13:36 +0700

    menampilkan data dari youtube

--- wpu_portfolio/index.php
+++ wpu_portfolio/index.php
@@ -1,3 +1,29 @@
+<?php 
+function get_CURL($url)
+{
+
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); 
+  $result = curl_exec($curl);
+  curl_close($curl); 
+  
+  return json_decode($result, true);
+
+}
+
+$result = get_CURL('https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=UC5P2nb8DMBH4QagCGC0vdBw&key=AIzaSyCyonwWpIViudNluB1cgFEyF4QaJnLVFzM');
+
+$youtubeProfilePic = $result['items'][0]['snippet']['thumbnails']['medium']['url'];
+$channelName = $result['items'][0]['snippet']['title'];
+$subscriber = $result['items'][0]['statistics']['subscriberCount'];
+
+// letest video
+$urlLatestVideo = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyCyonwWpIViudNluB1cgFEyF4QaJnLVFzM&channelId=UC5P2nb8DMBH4QagCGC0vdBw&maxResults=1&order=date&part=snippet';
+$result = get_CURL($urlLatestVideo);
+$LatestVideo = $result ['items'][0]['id']['videoId'];
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -82,17 +108,18 @@
       <div class="col-md-5">
         <div class="row">
           <div class="col-md-4">
-            <img src="img/profile1.png" width="200" class="rounded-circle img-thumbnail">
+            <img src="<?= $youtubeProfilePic; ?>" width="200" class="rounded-circle img-thumbnail">
           </div>
           <div class="col-md-8">
-            <h5>WebProgrammingFANI</h5>
-            <p>1000 Subscribers.</p>
+            <h5><?= $channelName; ?>I</h5>
+            <p><?= $subscriber; ?> Subscribers. </p>
+            <div class="g-ytsubscribe" data-channelid="UC5P2nb8DMBH4QagCGC0vdBw" data-layout="default" data-count="default"></div>
           </div>
         </div>
         <div class="row mt-3 pb-3">
           <div class="col">
             <div class="embed-responsive embed-responsive-16by9">
-              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/3jWOpibfjRc?rel=0" allowfullscreen></iframe>
+              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/<?= $LatestVideo?>rel=0" allowfullscreen></iframe>
             </div>
           </div>
         </div>
@@ -274,5 +301,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <script src="https://apis.google.com/js/platform.js"></script>
   </body>
 </html>
